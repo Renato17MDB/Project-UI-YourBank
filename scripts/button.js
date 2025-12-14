f847e3efe7e5626dd101dc02074a454cdc92fd7e
Renato17MDB <renato.md96@gmail.com>
2025-12-13 22:36:33 -0300

    feat: add header

---
+++ scripts/button.js
@@ -0,0 +1,6 @@
+const btnMenu = document.querySelector('.btn-menu');       
+const menuList = document.querySelector ('.menu-list');
+
+    btnMenu.addEventListener('click',() =>{
+    menuList.classList.toggle('show');
+});
