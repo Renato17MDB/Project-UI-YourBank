89231340fb0efa00edee44f8c4d1aa4acab1e6ee
Renato17MDB <renato.md96@gmail.com>
2026-01-12 12:00:01 -0300

    Add new section and improve layout structure

--- scripts/button.js
+++ scripts/button.js
@@ -6,3 +6,17 @@ const menuList = document.querySelector ('.menu-list');
     btnMenu.addEventListener('click',() =>{
     menuList.classList.toggle('show');
 });
+
+/*LOAD FAQ BTN*/
+const faqBtn = document.querySelector('.btn-load-faq');
+const containerFaq = document.querySelector('.items-container');
+const text = faqBtn.querySelector('.text-btn');
+
+faqBtn.addEventListener('click', () => {
+    const isOpen = containerFaq.classList.toggle('is-open');
+    faqBtn.classList.toggle('is-open');
+
+    faqBtn.setAttribute('aria-expanded', isOpen);
+
+    text.textContent = isOpen ? "Collapse FAQ's" : "Load All FAQ's";
+});
